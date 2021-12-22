8b7563bce156a2ab0e7701df0c654f099c691ccd
ZeKai Chong <yokai.czk@gmail.com>
2021-12-23 03:39:23 +1030

    Write into maincpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "GAME_H"
+using namespace std;
+
+int main(){
+    cout << "Welcome to guess the number." << endl;
+    cout << "Guess the number between 1 to 100" << endl;
+
+
+    return 0;
+}
