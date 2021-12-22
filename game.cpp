58b1873f259586a4f9bf5c91097adb91ac01c388
DowsonKerk <74602619+DowsonKerk@users.noreply.github.com>
2021-12-23 02:04:53 +0800

    Upgrade

--- game.cpp
+++ game.cpp
@@ -4,14 +4,18 @@ using namespace std;
 bool gameplay(int random){
     int chance = 6;
     int input;
+    int minimum = 1;
+    int maximum = 100;
     while (chance > 0){
-        cout << "Enter Number betweeen 1-100: ";
+        cout << "Enter Number betweeen " << minimum << " - " << maximum << ": ";
         cin >> input;
         if (input > random){
             cout << input << " is bigger than answer." << endl;
+            maximum = input - 1;
         } 
         else if (input < random){
             cout << input << " is smaller than answer." << endl;
+            minimum = input + 1;
         }
         else {
             return true;
