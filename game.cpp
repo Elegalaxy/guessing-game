59e60f4b2c86b61d3c97e6d5f1c2fbed114b6a84
DowsonKerk <74602619+DowsonKerk@users.noreply.github.com>
2021-12-23 01:52:49 +0800

    Debug

--- game.cpp
+++ game.cpp
@@ -5,10 +5,18 @@ bool gameplay(int random){
     int chance = 6;
     int input;
     while (chance > 0){
-        cout << "Enter Number betweeen 1-100:";
+        cout << "Enter Number betweeen 1-100: ";
         cin >> input;
-        if (input > random)
+        if (input > random){
+            cout << input << " is bigger than answer." << endl;
+        } 
+        else if (input < random){
+            cout << input << " is smaller than answer." << endl;
+        }
+        else {
+            return true;
+        }
+        chance --;
     }
-
-
+    return false;
 }
