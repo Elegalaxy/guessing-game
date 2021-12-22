36045e1599c2bb090f3fe861e2a6a4689c196349
ZeKai Chong <yokai.czk@gmail.com>
2021-12-23 04:04:22 +1030

    Add answer var

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 #include "game.h"
 using namespace std;
 
 int main(){
+    srand (time(NULL));
+    int ans = rand()%100+1;
+    
     cout << "Welcome to guess the number." << endl;
     cout << "Guess the number between 1 to 100 and you have " << endl;
 
-    if(gameplay())
+    if(gameplay(ans))
         cout << "You Win!" << endl;
     else
         cout << "You Loss!" << endl;
